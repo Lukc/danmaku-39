4e0161a3f30c6dc670e2bb8d33aa923ffc5827f0
Luka Vandervelden <lukc@upyum.com>
2017-06-16 17:24:11 +0200

    Windows-related workaround.

--- main.lua
+++ main.lua
@@ -1,5 +1,15 @@
 
 require "moonscript"
 
+-- It’s a hack, and will have to be dealt with properly at some point.
+if love.system.getOS() == "Windows" then
+	oldOpen = io.open
+	io.open = function(file, ...)
+		file = love.filesystem.getSourceBaseDirectory() .. "/danmaku-39/" .. file
+
+		return oldOpen(file, ...)
+	end
+end
+
 require "game"
 
